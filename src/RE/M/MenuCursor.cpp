c1a20597dd68ae2397290a2b7fb40e1a73b82aca
Alan Tse <alandtse@gmail.com>
2025-07-13 19:19:28 -0700

    fix(MenuCursor): fix runtime data use

--- src/RE/M/MenuCursor.cpp
+++ src/RE/M/MenuCursor.cpp
@@ -11,14 +11,15 @@ namespace RE
 
 	void MenuCursor::SetCursorVisibility(bool a_visible)
 	{
-		if (a_visible && showCursorCount < 0) {
+		auto& runtimeData = GetRuntimeData();
+		if (a_visible && runtimeData.showCursorCount < 0) {
 			do {
-				showCursorCount = REX::W32::ShowCursor(true);
-			} while (showCursorCount < 0);
-		} else if (!a_visible && showCursorCount >= 0) {
+				runtimeData.showCursorCount = REX::W32::ShowCursor(true);
+			} while (runtimeData.showCursorCount < 0);
+		} else if (!a_visible && runtimeData.showCursorCount >= 0) {
 			do {
-				showCursorCount = REX::W32::ShowCursor(false);
-			} while (showCursorCount >= 0);
+				runtimeData.showCursorCount = REX::W32::ShowCursor(false);
+			} while (runtimeData.showCursorCount >= 0);
 		}
 	}
 }
