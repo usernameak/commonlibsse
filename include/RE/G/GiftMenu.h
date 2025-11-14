cb3cbea05c93721ef2a6dd27fd8c16aa8d1c8fa4
Vermunds <33235701+Vermunds@users.noreply.github.com>
2025-11-14 12:33:38 +0100

    GiftMenu additions

--- include/RE/G/GiftMenu.h
+++ include/RE/G/GiftMenu.h
@@ -6,6 +6,7 @@
 
 namespace RE
 {
+	class BGSListForm;
 	struct ItemCard;
 	struct ItemList;
 
@@ -26,18 +27,26 @@ namespace RE
 		UI_MESSAGE_RESULTS ProcessMessage(UIMessage& a_message) override;    // 04
 		void               PostDisplay() override;                           // 06
 
-		[[nodiscard]] static RefHandle GetTargetRefHandle();
+		[[nodiscard]] static RefHandle    GetGifterRefHandle();
+		[[nodiscard]] static RefHandle    GetReceiverRefHandle();
+		[[nodiscard]] static bool         GetShowStolenItems();
+		[[nodiscard]] static bool         GetUseFavorPoints();  // always false regardless of the argument value in ShowGiftMenu papyrus function
+		[[nodiscard]] static BGSListForm* GetFilterList();
+
+		bool IsPlayerGifting();
+		bool IsPlayerReceiving();
 
 		// members
-		GFxValue        root;             // 30 - "Menu_mc"
-		ItemList*       itemList;         // 48
-		ItemCard*       itemCard;         // 50
-		BSTArray<void*> unk58;            // 58
-		std::uint64_t   unk70;            // 70
-		bool            pcControlsReady;  // 78
-		std::uint8_t    pad79;            // 79
-		std::uint16_t   pad7A;            // 7A
-		std::uint32_t   pad7C;            // 7C
+		GFxValue        root;                   // 30 - "Menu_mc"
+		ItemList*       itemList;               // 48
+		ItemCard*       itemCard;               // 50
+		BSTArray<void*> unk58;                  // 58
+		std::int32_t   totalValueTransferred;  // 70 - gold value, return value of ShowGiftMenu papyrus function
+		std::uint32_t   unk74;                  // 74
+		bool            pcControlsReady;        // 78
+		std::uint8_t    pad79;                  // 79
+		std::uint16_t   pad7A;                  // 7A
+		std::uint32_t   pad7C;                  // 7C
 	};
 	static_assert(sizeof(GiftMenu) == 0x80);
 }
--- src/RE/G/GiftMenu.cpp
+++ src/RE/G/GiftMenu.cpp
@@ -1,10 +1,56 @@
 #include "RE/G/GiftMenu.h"
+#include "RE/P/PlayerCharacter.h"
+#include "RE/T/TESObjectREFR.h"
 
 namespace RE
 {
-	RefHandle GiftMenu::GetTargetRefHandle()
+	RefHandle GiftMenu::GetGifterRefHandle()
 	{
 		static REL::Relocation<RefHandle*> handle{ RELOCATION_ID(519570, 406111) };
 		return *handle;
+	}
+
+	RefHandle GiftMenu::GetReceiverRefHandle()
+	{
+		static REL::Relocation<RefHandle*> handle{ RELOCATION_ID(0, 406112) };
+		return *handle;
+	}
+
+	bool GiftMenu::GetShowStolenItems()
+	{
+		static REL::Relocation<bool*> showStolenItems{ RELOCATION_ID(0, 406086) };
+		return *showStolenItems;
+	}
+
+	bool GiftMenu::GetUseFavorPoints()
+	{
+		static REL::Relocation<bool*> useFavorPoints{ RELOCATION_ID(0, 382811) };
+		return *useFavorPoints;
+	}
+
+	BGSListForm* GiftMenu::GetFilterList()
+	{
+		static REL::Relocation<BGSListForm**> filterList{ RELOCATION_ID(0, 406085) };
+		return *filterList;
+	}
+
+	bool GiftMenu::IsPlayerGifting()
+	{
+		RE::TESObjectREFRPtr refptr = nullptr;
+
+		if (RE::TESObjectREFR::LookupByHandle(GetGifterRefHandle(), refptr)) {
+			return refptr && refptr.get() == RE::PlayerCharacter::GetSingleton();
+		}
+		return false;
+	}
+
+	bool GiftMenu::IsPlayerReceiving()
+	{
+		RE::TESObjectREFRPtr refptr = nullptr;
+
+		if (RE::TESObjectREFR::LookupByHandle(GetReceiverRefHandle(), refptr)) {
+			return refptr && refptr.get() == RE::PlayerCharacter::GetSingleton();
+		}
+		return false;
 	}
 }
