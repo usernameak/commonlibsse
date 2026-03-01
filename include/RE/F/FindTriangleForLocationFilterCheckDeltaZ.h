63c865ac840546a7880d4c7dcfef74d2cc74f1ae
powerof3 <32599957+powerof3@users.noreply.github.com>
2026-03-01 22:15:59 +0000

    maintenance

--- include/RE/F/FindTriangleForLocationFilterCheckDeltaZ.h
+++ include/RE/F/FindTriangleForLocationFilterCheckDeltaZ.h
@@ -7,7 +7,7 @@ namespace RE
 	public:
 		inline static constexpr auto RTTI = RTTI_FindTriangleForLocationFilter;
 		inline static constexpr auto VTABLE = VTABLE_FindTriangleForLocationFilter;
-		
+
 		virtual ~FindTriangleForLocationFilter();  // 00
 
 		// add
@@ -20,14 +20,14 @@ namespace RE
 	public:
 		inline static constexpr auto RTTI = RTTI_FindTriangleForLocationFilterCheckDeltaZ;
 		inline static constexpr auto VTABLE = VTABLE_FindTriangleForLocationFilterCheckDeltaZ;
-		
+
 		FindTriangleForLocationFilterCheckDeltaZ() :
 			maxDistAbove(std::numeric_limits<float>::max()),
 			maxDistBelow(std::numeric_limits<float>::max())
 		{
 			stl::emplace_vtable(this);
 		}
-		
+
 		~FindTriangleForLocationFilterCheckDeltaZ() override = default;  // 00
 
 		// add
